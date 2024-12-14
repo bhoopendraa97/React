981330ec0c945f1e831bfd6689278ee163ec119f
bhoopendraa97 <bhoopendraa77@gmail.com>
2024-12-14 16:14:47 +0530

    space added

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import CounterContext from './CounterContext.jsx'
+
 createRoot(document.getElementById('root')).render(
   <CounterContext>
     <App />
